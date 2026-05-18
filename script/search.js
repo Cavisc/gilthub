b6ed8e87a6a21290e3b988def9cc370cbde1c4e6
Cavisc <cavisc.icm@gmail.com>
2026-05-18 00:33:47 -0300

    Add cta-search priority

--- script/search.js
+++ script/search.js
@@ -1,11 +1,26 @@
 document.addEventListener("keydown", (event) => {
   if (event.key === "/") {
     const searchInput = document.getElementById("search");
+    const ctaSearchInput = document.getElementById("cta-search");
     const activeElement = document.activeElement;
 
-    if (searchInput && (activeElement !== searchInput || !activeElement)) {
+    console.log(activeElement);
+
+    if (
+      searchInput &&
+      (!activeElement ||
+        (activeElement !== searchInput && activeElement !== ctaSearchInput))
+    ) {
       event.preventDefault();
-      searchInput.focus();
+
+      searchInput.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+
+      setTimeout(() => {
+        searchInput.focus();
+      }, 100);
     }
   }
 });
